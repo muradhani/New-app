de4a98c50f9740ed0b87e9942c3e23e07f5a7c06
moura <muradhani>
2023-05-23 22:43:26 +0300

    database error solved

--- app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/db/ArticleDatabase.kt
@@ -9,7 +9,7 @@ import com.androiddevs.mvvmnewsapp.models.Article
 
 @Database(
     entities = [Article::class],
-    version = 1
+    version = 2
 )
 @TypeConverters(Converters::class)
 abstract class ArticleDatabase :RoomDatabase(){
@@ -28,7 +28,8 @@ abstract class ArticleDatabase :RoomDatabase(){
                 context.applicationContext,
                 ArticleDatabase ::class.java,
                 "article_db.db"
-            ).build()
+
+            ).fallbackToDestructiveMigration().build()
 
     }
 }
